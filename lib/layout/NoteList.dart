a167f95acb4f38e4b65b4e45ac450584ad761059
paveltech <pavelcep.sust11@gmail.com>
2020-08-07 15:47:43 +0600

    node modify added

--- lib/layout/NoteList.dart
+++ lib/layout/NoteList.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_rest_api_example/layout/NoteModify.dart';
 import 'package:flutter_rest_api_example/models/NoteForListing.dart';
 
 class NoteList extends StatelessWidget {
-
-
   final note = [
     new NoteForListing('1', 'Hello 1', DateTime.now(), DateTime.now()),
     new NoteForListing('1', 'Hello 1', DateTime.now(), DateTime.now()),
@@ -15,7 +14,6 @@ class NoteList extends StatelessWidget {
     new NoteForListing('1', 'Hello 1', DateTime.now(), DateTime.now()),
   ];
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,7 +21,10 @@ class NoteList extends StatelessWidget {
           title: Text('Note List'),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {},
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => NoteModify()));
+          },
           child: Icon(Icons.add),
         ),
         body: ListView.separated(
@@ -33,9 +34,8 @@ class NoteList extends StatelessWidget {
                   note[position].noteTitle,
                   style: TextStyle(color: Theme.of(context).primaryColor),
                 ),
-                subtitle: Text(
-                  '${formateDateTime(note[position].lastEditDateTime)}'
-                ),
+                subtitle:
+                    Text('${formateDateTime(note[position].lastEditDateTime)}'),
               );
             },
             separatorBuilder: (_, __) => Divider(
@@ -44,9 +44,8 @@ class NoteList extends StatelessWidget {
                 ),
             itemCount: note.length));
   }
-  
-  String formateDateTime(var dateTime){
+
+  String formateDateTime(var dateTime) {
     return '${dateTime.day}/${dateTime.month}/${dateTime.year}';
   }
 }
-
