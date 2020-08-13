fe2c8133d480c00da40bbb9f0f0c7af33bca5253
paveltech <pavelcep.sust11@gmail.com>
2020-08-13 19:51:08 +0600

    favorite screen

---
+++ lib/bloc/ui/favorite_screen.dart
@@ -0,0 +1,25 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_rest_api_example/bloc/BLoC/bloc_provider.dart';
+
+class FavoriteScreen extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+
+}
+
+
+class _FavoriteScreen extends State<FavoriteScreen>{
+
+
+  @override
+  Widget build(BuildContext context) {
+    var bloc = BlockProvider.of(context);
+
+  }
+
+}
