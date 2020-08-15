853e664458339f8e0918ba0f0da389a1da88b3fb
paveltech <pavelcep.sust11@gmail.com>
2020-08-15 14:42:23 +0600

    restaurant screen ready

---
+++ lib/bloc/ui/restaurant_screen.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_rest_api_example/bloc/BLoC/bloc_provider.dart';
+import 'package:flutter_rest_api_example/bloc/BLoC/restaurant_bloc.dart';
+import 'package:flutter_rest_api_example/bloc/pojo/location.dart';
+import 'package:flutter_rest_api_example/bloc/pojo/restaurant.dart';
+import 'package:flutter_rest_api_example/bloc/ui/favorite_screen.dart';
+import 'package:flutter_rest_api_example/bloc/ui/restaurant_tile.dart';
+
+class RestaurantScreen extends StatelessWidget {
+  final Location location;
+
+  const RestaurantScreen({this.location});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(location.title),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.favorite_border),
+            onPressed: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (_) => FavoriteScreen()));
+            },
+          )
+        ],
+      ),
+      body: _buildSearch(context),
+    );
+  }
+
+  Widget _buildSearch(BuildContext context) {
+    final bloc = RestaurantBloc(location);
+    return BlockProvider(
+      bloc: bloc,
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'What do you want to eat?'),
+              onChanged: (query) {
+                bloc.submitQuery(query);
+              },
+            ),
+          ),
+          Expanded(
+            child: _buildStreamBuilder(bloc),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildStreamBuilder(RestaurantBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.stream,
+      builder: (context, snapshot) {
+        final results = snapshot.data;
+
+        if (results == null) {
+          return Center(child: Text('Enter a restaurant name'));
+        }
+
+        if (results.isEmpty) {
+          return Center(child: Text('No result'));
+        }
+
+        return _buildSearchResults(results);
+      },
+    );
+  }
+
+  Widget _buildSearchResults(List<Restaurant> results) {
+    return ListView.separated(
+      itemCount: results.length,
+      separatorBuilder: (context, index) => Divider(),
+      itemBuilder: (context, index) {
+        final restaurant = results[index];
+        return RestaurantTile(restaurant: restaurant);
+      },
+    );
+  }
+}
